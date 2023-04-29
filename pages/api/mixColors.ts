9aa782b6b191965049077afdd7cf3fbf43ac3af7
Gordon Lewis <glewis@berkeley.edu>
2023-04-29 15:55:01 -0700

    adds message to the conditional logic

--- pages/api/mixColors.ts
+++ pages/api/mixColors.ts
@@ -44,6 +44,7 @@ export default async function (
       messages: [{role: "assistant", content: prompt}]
     });
     const jsonResponse = response && response.data && response.data.choices && response.data.choices[0] && response.data.choices[0].message ? JSON.parse(response.data.choices[0].message.content.trim()) : null;
+
     res.status(200).json(jsonResponse);
   } catch (error) {
     // Consider adjusting the error handling logic for your use case
